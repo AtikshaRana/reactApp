10c2ae9dcd9b8f03982bba9974085060690e5bae
Atiksha <atiksha@webenertia.com>
2024-04-11 14:11:29 +0530

    Update .

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,5 @@ import App from './App'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-    <h1 className='text-red'>hiii</h1>
   </React.StrictMode>,
 )
